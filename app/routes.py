6d5131b355729835f41bae1001a674b0d2bb77e9
Pawel Dabrowski <id858105@belboneii.dc>
2020-01-21 10:30:03 +0100

    fixed current_user syntax

--- app/routes.py
+++ app/routes.py
@@ -24,7 +24,7 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-	if current_user is authenticated:
+	if current_user.is_authenticated:
 		return redirect(url_for('index'))
 	form = LoginForm()
 	if form.validate_on_submit():
